b3bd69f7257fde894e86aa938409379a321ae487
Victor <victor.lvl47@gmail.com>
2022-04-06 18:39:16 -0600

    Refactored main.js to a single scroll event

--- main.js
+++ main.js
@@ -17,44 +17,43 @@ function setCurrentYearInFooter() {
 // been fully loaded and parsed, without waiting for
 // stylesheets, images, and subframes to finish loading.
 document.addEventListener('DOMContentLoaded', (e) => {
-    console.log(`Hi! Welcome to my website.`);
+  console.log(`Hi! Welcome to my website.`);
 
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-          $('#navbar')
-            .addClass('navbar-scrolled');
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav');
+
+
+  $(window).on('scroll', function() {
+
+    if ($(this).scrollTop() > 100) {
+      $('#navbar').addClass('navbar-scrolled');
+    }
+    else {
+      $('#navbar').removeClass('navbar-scrolled');
+    }
+
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+          bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
       }
-      else {
-        $('#navbar')
-          .removeClass(
-            'navbar-scrolled');
-        }
-    });
+      
+      if (cur_pos < 300) {
+        $(".nav ul:first li:first").addClass('active');
+      }
+    }); // nav_sections.each END
 
-    // Navigation active state on scroll
-    var nav_sections = $('section');
-    var main_nav = $('.nav');
+  }); // $(window).on('scroll', ...) END
 
-    $(window).on('scroll', function() {
-      var cur_pos = $(this).scrollTop() + 200;
-
-      nav_sections.each(function() {
-        var top = $(this).offset().top,
-            bottom = top + $(this).outerHeight();
-
-        if (cur_pos >= top && cur_pos <= bottom) {
-          if (cur_pos <= bottom) {
-            main_nav.find('li').removeClass('active');
-          }
-          main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-        }
-        if (cur_pos < 300) {
-          $(".nav ul:first li:first").addClass('active');
-        }
-      }); // nav_sections.each END
-    }); // $(window).on('scroll', ...) END
-
-    // Set to the current year in the footer section of the website
-    setCurrentYearInFooter();
+  // Set to the current year in the footer section of the website
+  setCurrentYearInFooter();
 
 }); // DOMContentLoaded END
