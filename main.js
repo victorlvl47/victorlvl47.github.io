6be362aaf1c657d0c4cd0ff7a0e76e2fb1baac7b
Victor <victor.lvl47@gmail.com>
2022-04-20 06:45:20 -0600

    Refactored the code that creates a project tile into a function

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ function whenScrolledDownChangeNavbarColor() {
   }
 }
 
+
+
+
 // highligt the current navbar item, depending of the
 // section the user is currently on.
 function highlightCurrentSectionInNavbar(nav_sections, main_nav) {
@@ -28,6 +31,9 @@ function highlightCurrentSectionInNavbar(nav_sections, main_nav) {
 
 }
 
+
+
+
 // Set to the current year in the footer section
 function setCurrentYearInFooter() {
   // Get the current year
@@ -39,6 +45,55 @@ function setCurrentYearInFooter() {
   // set the current year in the <span id="current-year"> element
   current_year_span.innerText = CURRENT_YEAR
 } // setCurrentYearInFooter END
+
+
+
+
+// Create the project item and add it to the website
+function createProjectItem(result) {
+
+  // Create a <a></a> element with a link to the current project
+  var $project_element_link = $('<a>', {href: result.data[6].link_to_project,
+                                        target: "_blank",
+                                        class: "project"});
+
+  // create the <div> element that goes inside the <a> element.
+  var $portafolio_block_element = $('<div>', {class: "portfolio-block"});
+  $project_element_link.append($portafolio_block_element);
+
+  // create the <img> element, the image for the project.
+  var $project_image_element = $('<img>', {class: "project-image",
+                                           src: result.data[6].image_src,
+                                           alt: "project"});
+  // Add the image to the <div class='portafolio-block'> element.
+  $portafolio_block_element.append($project_image_element);
+
+  // Create the <div class='caption'> element
+  var $caption_element = $('<div>', {class: "caption"});
+  $portafolio_block_element.append($caption_element);
+
+  // Create the external link icon.
+  var $external_link_icon = $('<p class="search-icon"><i class="fa fa-external-link-square-alt"></i></p><h4>Go to Project</h4>');
+  $caption_element.append($external_link_icon);
+
+  // Create the project description element <p class="project-description">.
+  var $project_description = $('<p>', {class: "project-description", text: result.data[6].project_description});
+  $caption_element.append($project_description);
+
+  // Create the <p class="project-title"> for the project title
+  var $project_title_element = $('<p>', {class: "project-title"});
+  $project_title_element.append('<span class="code">&#91;</span>');
+  $project_title_element.append(result.data[6].project_title);
+  $project_title_element.append('<span class="code">&#93;</span>');
+  // Append to the project link
+  $project_element_link.append($project_title_element);
+
+  console.log($project_element_link);
+
+  $('.projects-grid').append($project_element_link);
+} // END createProjectItems
+
+
 
 
 // Listen for "DOMContentLoaded" event.
@@ -65,45 +120,20 @@ document.addEventListener('DOMContentLoaded', (e) => {
       console.log("Success, we got the data about the projects.");
       console.table(result.data[6]);
 
-      // Create a <a></a> element with a link to the current project
-      var $project_element_link = $('<a>', {href: result.data[6].link_to_project,
-                                            target: "_blank",
-                                            class: "project"});
+      // Create the project item and add it to the website
+      createProjectItem(result);
 
-      // create the <div> element that goes inside the <a> element.
-      var $portafolio_block_element = $('<div>', {class: "portfolio-block"});
-      $project_element_link.append($portafolio_block_element);
+      $('.projects-grid').btnLoadmore({
 
-      // create the <img> element, the image for the project.
-      var $project_image_element = $('<img>', {class: "project-image",
-                                               src: result.data[6].image_src,
-                                               alt: "project"});
-     // Add the image to the <div class='portafolio-block'> element.
-     $portafolio_block_element.append($project_image_element);
+                   showItem : 6,
 
-     // Create the <div class='caption'> element
-     var $caption_element = $('<div>', {class: "caption"});
-     $portafolio_block_element.append($caption_element);
+                   whenClickBtn : 3,
 
-     // Create the external link icon.
-     var $external_link_icon = $('<p class="search-icon"><i class="fa fa-external-link-square-alt"></i></p><h4>Go to Project</h4>');
-     $caption_element.append($external_link_icon);
+                   textBtn : 'Load More',
 
-     // Create the project description element <p class="project-description">.
-     var $project_description = $('<p>', {class: "project-description", text: result.data[6].project_description});
-     $caption_element.append($project_description);
+                   classBtn : 'btn btn-show-all'
 
-     // Create the <p class="project-title"> for the project title
-     var $project_title_element = $('<p>', {class: "project-title"});
-     $project_title_element.append('<span class="code">&#91;</span>');
-     $project_title_element.append(result.data[6].project_title);
-     $project_title_element.append('<span class="code">&#93;</span>');
-     // Append to the project link
-     $project_element_link.append($project_title_element);
-
-      console.log($project_element_link);
-
-      $('.projects-grid').append($project_element_link);
+               });
 
 
     },
@@ -112,25 +142,6 @@ document.addEventListener('DOMContentLoaded', (e) => {
       console.log(error);
     }
   });
-
-
-  // Handle click on "Load more" button
-  //$('#btn-load-more').on('click', function(){
-      // Load more data
-      //console.log("Load More button clicked!");
-   //});
-
-   $('.projects-grid').btnLoadmore({
-
-                showItem : 6,
-
-                whenClickBtn : 3,
-
-                textBtn : 'Load More',
-
-                classBtn : 'btn btn-show-all'
-
-            });
 
 
   // When the user scrolls the website
